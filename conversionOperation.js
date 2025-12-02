43bb2cdf75bf68d2b9cab09f88a33556ae9b4dde
Shayan <shanich03447@gmail.com>
2025-12-02 15:09:47 +0500

    conversionOperation

---
+++ conversionOperation.js
@@ -0,0 +1,24 @@
+let score= 33;
+let age = "33abc";
+// console.log(typeof score);
+// console.log(typeof age);
+// Convert the string into number
+let valueInNumber=Number(age);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" converted into numbers
+// "33abc" converted into NAN
+// true converted into 1
+// false converted into 0
+let isLoggedIn="Shayan";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+// 1 => true
+//  0 => false
+// " " => false
+// "Name" => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
