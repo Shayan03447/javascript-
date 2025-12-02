e50ce4aed81cec090f47b90c18e6543d98631585
Shayan <shanich03447@gmail.com>
2025-12-02 17:01:50 +0500

    Datatypes_summary

---
+++ datatype_summary.js
@@ -0,0 +1,9 @@
+// Premitive datatype
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference (Non Premitive)
+//  Array, object, functions
+const id = Symbol('123')
+const anotherId =  Symbol('112233')
+console.log(typeof id);
+console.log(typeof anotherId)
