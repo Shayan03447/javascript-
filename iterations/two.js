d9c26142134c3f021ed788a64d2918d8aace5ee5
Shayan <shanich03447@gmail.com>
2025-12-09 00:02:36 +0500

    update

---
+++ iterations/two.js
@@ -0,0 +1,42 @@
+//let index=0
+//while (index <= 10) {
+    //console.log(`value of index is ${index}`)
+  //  index+=2
+//}
+
+
+// let myarray=["Superman","Batman","Spiderman"]
+// let index=0
+// while (index <myarray.length) {
+//     console.log(`The value of array is ${myarray[index]}`)
+//     index=index+1
+    
+// }
+
+// let score=1
+// do {
+//     console.log(`Score is ${score}`)
+//     score++
+// } while (score<=10);
+
+// let myarray1=[1,2,3,4,5,6,7,8,9]
+// let i=0
+// while (i<myarray1.length) {
+//     console.log(`The value of i is ${myarray1[i]}`)
+//     i++
+    
+// }
+// let number=0
+// do {
+//     console.log(`number is ${number}`)
+//     number+=1
+// } while (number<=10);
+
+let arr=[1,2,3,4,5,6,7,8,9,10]
+for (const i of arr) {
+   // console.log(i)
+}
+let string1="Hellow world"
+for(const character of string1){
+    console.log(character)
+}
